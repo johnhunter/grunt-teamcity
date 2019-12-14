d735e2d994028cd19ca93d9be09ee77ef82ed7f1
John Hunter <john.hunter@apperio.com>
2019-12-14 16:21:28 +0000

    Update for ES6 and node version bump

--- tasks/teamcity.js
+++ tasks/teamcity.js
@@ -10,14 +10,49 @@
 'use strict';
 
 // Nodejs libs.
-var util = require('util');
-var fs = require('fs');
+const util = require('util');
+const fs = require('fs');
+
+
+const escape = text => {
+  return (text || '').replace("'","|'").replace("\n","|n").replace("\r","|r").replace("\f","|f");
+};
+
+const tcFormat = (text, status, errorDetails) => {
+  if (text.indexOf('##teamcity[') !== -1) {
+    return text;
+  }
+
+  return  ("##teamcity[message text='" + escape(text) +
+    "' errorDetails='" + escape(errorDetails || '') +
+    "' status='" + escape(status) + "']\n").grey;
+};
+
+const writeSync = text => fs.writeSync(process.stdout.fd,text);
+
+const writeMsg = (msg, status) => {
+  if (msg) {
+    writeSync(tcFormat(msg, status));
+  } else {
+    writeSync(tcFormat(status, status));
+  }
+};
+
+// verbatum from grunt.log
+const format = args => {
+  // Args is a argument array so copy it in order to avoid wonky behavior.
+  args = [].slice.call(args, 0);
+  if (args.length > 0) {
+    args[0] = String(args[0]);
+  }
+  return util.format.apply(util, args);
+};
 
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('teamcity', 'Send log warnings in TeamCity service message format.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
+    const options = this.options({
       suppressGruntLog: false,
       status: {
         // TC WARNING does not trigger a task fail
@@ -27,8 +62,8 @@ module.exports = function(grunt) {
       }
     });
 
-    var log = grunt.log;
-    var _log = {
+    const log = grunt.log;
+    const _log = {
       warn: log.warn,
       error: log.error,
       errorlns: log.errorlns,
@@ -36,70 +71,32 @@ module.exports = function(grunt) {
     };
 
 
-    log.warn = function() {
+    log.warn = (...args) => {
       if (!options.suppressGruntLog) {
-        _log.warn.apply(log, arguments);
+        _log.warn.apply(log, args);
       }
-      writeMsg(format(arguments), options.status.warning);
+      writeMsg(format(args), options.status.warning);
     };
 
-    log.error = function() {
+    log.error = (...args) => {
       if (!options.suppressGruntLog) {
-        _log.error.apply(log, arguments);
+        _log.error.apply(log, args);
       }
-      writeMsg(format(arguments), options.status.error);
+      writeMsg(format(args), options.status.error);
     };
 
-    log.errorlns = function() {
+    log.errorlns = (...args) => {
       if (!options.suppressGruntLog) {
-        _log.errorlns.apply(log, arguments);
+        _log.errorlns.apply(log, args);
       }
-      writeMsg(format(arguments), options.status.error);
+      writeMsg(format(args), options.status.error);
     };
 
-    log.fail = function() {
+    log.fail = (...args) => {
       if (!options.suppressGruntLog) {
-        _log.fail.apply(log, arguments);
+        _log.fail.apply(log, args);
       }
-      writeMsg(format(arguments), options.status.failure);
+      writeMsg(format(args), options.status.failure);
     };
-
-
-    function writeMsg (msg, status) {
-      if (msg) {
-        writeSync(tcFormat(msg, status));
-      } else {
-        writeSync(tcFormat(status, status));
-      }
-    }
-
-    function tcFormat(text, status, errorDetails) {
-      if (text.indexOf('##teamcity[') !== -1) {
-        return text;
-      }
-
-      return  ("##teamcity[message text='" + escape(text) +
-        "' errorDetails='" + escape(errorDetails || '') +
-        "' status='" + escape(status) + "']\n").grey;
-    }
-
-    function escape(text){
-      return (text || '').replace("'","|'").replace("\n","|n").replace("\r","|r").replace("\f","|f");
-    }
-
-    function writeSync(text){
-        fs.writeSync(process.stdout.fd,text);
-    }
-
-    // verbatum from grunt.log
-    function format(args) {
-      // Args is a argument array so copy it in order to avoid wonky behavior.
-      args = [].slice.call(args, 0);
-      if (args.length > 0) {
-        args[0] = String(args[0]);
-      }
-      return util.format.apply(util, args);
-    }
   });
-
 };
